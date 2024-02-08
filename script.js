6fabb0d7271b2da5ef2c8a6c0f7a86d3b737f3ce
chonkz198 <133202283+chonkz198@users.noreply.github.com>
2024-02-09 00:30:40 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,172 @@
+class Draw {
+  constructor(opts = {}) {
+    this.position = []
+    this.ctx = opts.ctx
+    this.freq = 0.00015
+    this.colors = ['white', '#99C5B5', '#AFECE7', '#81F499']
+  }
+
+  update() {
+  }
+
+  draw(ctx) {
+    ctx.save()
+    ctx.beginPath()
+    for (var i = 1; i < 6; i++) {
+      ctx.beginPath()
+      for (var x = 0; x < canvas.width; x++) {
+        if (moyenne < 10) {
+          moyenne = 1
+        }
+        var y = Math.sin(x * moyenne * (freq * 0.0000025) * (i / 3) + (cumul * 0.0005)) * (moyenne * 2.5)
+        ctx.lineTo(x, y + canvas.height / 2)
+      }
+      ctx.globalAlpha = i / 10 
+      ctx.strokeStyle = this.colors[i%2]
+      ctx.lineWidth = 3
+      ctx.stroke()
+    }
+    ctx.restore()
+
+  }
+} 
+
+//////////////////////////////
+
+document.getElementById('start').addEventListener("click", start)
+
+var canvas = document.getElementById("canvas")
+var ctx = canvas.getContext('2d')
+
+var audioCtx = new (window.AudioContext || window.webkitAudioContext)()
+var audioBuffer
+var audioSource
+var analyser = audioCtx.createAnalyser()
+var biquadFilter = audioCtx.createBiquadFilter()
+var frequencyData
+var frequencyIdx = Math.floor(1024)
+
+var t = 0
+var DELTA_TIME = 0
+var LAST_TIME = Date.now()
+
+var moyenne = 0
+var reducer = (acc, reducer) => acc + reducer
+var cumul = 0
+var average = 0
+var lines = []
+
+var slider = document.getElementById("range")
+var freq = 30
+
+var songUrl = '01.mp3'
+
+canvas.style.width = 105 + 'vw'
+canvas.style.height = 100  + 'vh'
+canvas.height = window.innerHeight
+canvas.width = window.innerWidth
+
+slider.oninput = function () {
+  freq = this.value
+}
+
+function loadSound(url) {
+  var request = new XMLHttpRequest();
+  request.open('GET', url, true)
+  request.responseType = 'arraybuffer'
+
+  request.onload = function () {
+    audioCtx.decodeAudioData(request.response, function (buffer) {
+      // success callback
+      audioBuffer = buffer
+      // Create sound from buffer
+      audioSource = audioCtx.createBufferSource()
+      audioSource.buffer = audioBuffer
+      // connect the audio source to context's output
+      audioSource.connect(analyser)
+      //analyser.fftSize = 256
+      analyser.connect(audioCtx.destination)
+      frequencyData = new Uint8Array(analyser.frequencyBinCount)
+      // Filters
+      audioSource.connect(biquadFilter)
+      biquadFilter.connect(audioCtx.destination)
+      // play sound
+      audioSource.start()
+      frame()
+    }, function () {
+      // error callback
+      //
+    })
+  }
+  request.send()
+}
+
+function initLine() {
+  lines = new Draw()
+}
+
+function frame() {
+  requestAnimationFrame(frame)
+
+  DELTA_TIME = Date.now() - LAST_TIME
+  LAST_TIME = Date.now()
+  t += 0.01
+
+  analyser.getByteFrequencyData(frequencyData)
+  ctx.clearRect(0, 0, canvas.width, canvas.height)
+  biquadFilter.type = "lowshelf"
+  biquadFilter.frequency.setValueAtTime(freq, audioCtx.currentTime)
+  biquadFilter.gain.value = 25
+
+  moyenne = frequencyData.reduce(reducer) / frequencyData.length
+
+  for (var i = 0; i < 6; i++) {
+    // get the frequency according to current i
+    let percentIdx = i / 6;
+    let frequencyIdx = Math.floor(1024 * percentIdx)
+
+    lines.update()
+    lines.draw(ctx)
+
+    cumul += frequencyData[frequencyIdx]
+  }
+  average = cumul / 255;
+}
+
+/*function init() {
+  initLine()
+  webAudioTouchUnlock(AudioCtx)
+  loadSound(songUrl)  
+}
+
+ init()*/
+if (audioCtx.state === 'suspended' && 'ontouchstart' in window) {
+  var unlock = function() {
+    audioCtx.resume().then(function() {
+      document.body.removeEventListener('touchstart', unlock);
+      document.body.removeEventListener('touchend', unlock);
+      setTimeout(function(){ 
+        document.getElementById("home").style.display = "none"
+        document.getElementById("app").style.display = "contents"
+        initLine()
+        loadSound(songUrl)
+      }, 3000);
+    });
+  };
+  document.body.addEventListener('touchstart', unlock, false);
+  document.body.addEventListener('touchend', unlock, false);
+}
+
+function start() {
+  document.getElementById("home").style.display = "none"
+  document.getElementById("app").style.display = "contents"
+  initLine()
+  loadSound(songUrl)
+}
+document.querySelector('.iframe').addEventListener('click', function(event) {
+    event.preventDefault(); // Prevent the default action
+    const pdfUrl = this.getAttribute('href'); // Get the URL of the PDF
+    const iframe = document.querySelector('#pdfIframe');
+    iframe.src = pdfUrl; // Set the iframe source to the PDF URL
+    document.querySelector('#iframeContainer').classList.remove('hidden'); // Show the iframe
+});
